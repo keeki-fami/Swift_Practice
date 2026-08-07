7e4034483a35662b36c624758a2c08165c2ce13b
keeki-fami <towatowa3939keeki@gmail.com>
2026-08-07 09:42:17 +0900

    Sequence

---
+++ sequence/sequence.swift
@@ -0,0 +1,31 @@
+// Sequence protocol
+
+// for-in loopで各要素をiterateできる
+let oneTwoThree = 1...3
+for number in oneTwoThree {
+    print(number)
+}
+
+// デフォルトでcontainsメソッドが実装されている
+if oneTwoThree.contains(1) {print("Yes")} else {print("No")}
+
+struct Countdown: Sequence, IteratorProtocol {
+    var count: Int
+
+    mutating func next() -> Int? {
+	if count == 0 {
+	    return nil
+	} else {
+	    defer { count -= 1 }
+	    return count
+	}
+    }
+}
+
+let threeToGo = Countdown(count: 3)
+for i in threeToGo {
+    print(i)
+}
+
+// これもできる？
+if oneTwoThree.contains(4) {print("Yes")} else {print("No")}
