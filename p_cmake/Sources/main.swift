729db009e4a3d1e881d211a8ec1171bd34b50c82
keeki-fami <towatowa3939keeki@gmail.com>
2026-08-13 11:10:37 +0900

    add swift with cmake

---
+++ p_cmake/Sources/main.swift
@@ -0,0 +1,7 @@
+print("hello from Swift built by CMake!")
+
+func add(_ lhs: Int, _ rhs: Int) -> Int {
+    lhs + rhs
+}
+
+print("20 + 22 = \(add(20, 22))")
---
+++ p_cmake/build/CMakeFiles/4.0.3/CMakeSwiftCompiler.cmake
@@ -0,0 +1,18 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+set(CMAKE_Swift_COMPILER "/usr/bin/swiftc")
+set(CMAKE_Swift_COMPILER_ID "Apple")
+set(CMAKE_Swift_COMPILER_VERSION "6.3.3")
+
+set(CMAKE_Swift_COMPILER_LOADED 1)
+set(CMAKE_Swift_COMPILER_WORKS "TRUE")
+
+set(CMAKE_Swift_COMPILER_ENV_VAR "SWIFTC")
+
+set(CMAKE_Swift_COMPILER_ID_RUN 1)
+set(CMAKE_Swift_SOURCE_FILE_EXTENSIONS swift)
+
+set(CMAKE_Swift_COMPILER_USE_OLD_DRIVER "FALSE")
+
+set(CMAKE_Swift_IMPLICIT_INCLUDE_DIRECTORIES "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include")
